0789c48ca2350c497f998bb8cc22aee158c6c08b
Jackelyne Nunes <jackelynenunes@outlook.com>
2023-10-27 18:35:33 -0300

    feat(navbar): Add navbar, tailwind and icons

---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-app-bar 
+      flat 
+      app
+      color="#111111"
+    >
+      <v-btn icon>
+        <v-icon icon="fas fa-cat" />
+      </v-btn>
+      <v-spacer />
+        <v-btn
+          class="hidden md:block mr-8"
+          v-for="item in navigationItems"
+          :key="item.value"
+          @click="scrollTo(item.value)"
+        >
+          {{ item.title }}
+        </v-btn>
+      <v-app-bar-nav-icon 
+        @click="isMenuOpen = !isMenuOpen"
+        class="md:hidden"
+      />
+    </v-app-bar>
+
+    <v-navigation-drawer 
+      app
+      v-model="isMenuOpen"
+      temporary
+      location="right"
+    >
+      <v-col 
+        v-for="item in navigationItems"
+        :key="item.value"
+      >
+        <p @click="scrollTo(item.value)"> {{ item.title }} </p>
+      </v-col>
+    </v-navigation-drawer>
+</template>
+  
+<script>
+  
+export default {
+  name: 'NavBar',
+  data: () => ({
+    title: ' My Portfolio',
+    isMenuOpen: false,
+    navigationItems: [
+      { title: 'Home', value: 'home' },
+      { title: 'About', value: 'about' },
+      { title: 'Projects', value: 'projects' },
+      { title: 'Contact', value: 'contact' }
+    ],
+  }),
+  methods: {
+    scrollTo(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+};
+</script>
+  
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,18 @@
-// Styles
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 import 'vuetify/styles'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+})
