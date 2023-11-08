7b596305b2b8ac77bcd0f04a668bbc407294a674
Jackelyne Nunes <jackelynenunes@outlook.com>
2023-11-07 22:12:24 -0300

    feat: starting home content view

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Hello! <br> I am Jackelyne, <br> a front-end developer.</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeContent',
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Playfair+Display:400,900');
+
+.container {
+	background:#151314;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	overflow: hidden;
+	position: relative;
+  min-width: 100%;
+}
+
+h1 {
+	font-family: 'Playfair Display', serif;
+	font-size: 6em;
+	letter-spacing: 2px;
+	font-weight: 900;
+	line-height: .9em;
+}
+</style>
